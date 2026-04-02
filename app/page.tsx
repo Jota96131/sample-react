13f2ed324964e2a3e2f15eb14e60f0e2a69a4701
Gushiken Jota <j13119613@gmail.com>
2026-04-02 13:27:16 +0900

    pageを追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,35 @@
-import Image from "next/image";
+import axios from "axios";
 
-export default function Home() {
+type QiitaResponse = {
+  id: string;
+  title: string;
+  url: string;
+  image: string;
+};
+
+export default async function Home() {
+  const getQiitaItems = async () => {
+    const response = await axios.get<QiitaResponse[]>(
+      "https://qiita.com/api/v2/items?query=user:Sicut_study&per_page=4",
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.QIITA_API_KEY}`,
+        },
+      },
+    );
+    return response.data;
+  };
+
+  const qiitaItems = await getQiitaItems();
+
   return (
     <div>
       <h1>Topページ</h1>
+      <ul>
+        {qiitaItems.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
